fa3f9c0f319257e87f5a9b46b363aeeb8ea86906
alinaberlin <alina.ghetler@gmail.com>
2016-03-06 23:10:21 +0100

    am adaugat contact si poza

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,7 @@
 
 
 var bio = {"name":"Alina Ghetler","role":"Junior Developer",
- "skils": "skils", "telefonNumber": "0123456789","email":"alina.ghetler@gmail.com", "pictureUrl": "http://t1.gstatic.com/images?q=tbn:ANd9GcRI6C_T0Gc7gfNXs-sfvYYqxeoH2Mb-KnTPSAEZ3miDFYqjIkMZw68P",
+ "skills": ["team spirit","enthusiast","eager to learn"], "telefonNumber": "0123456789","email":"alina.ghetler@gmail.com", "pictureUrl": "http://t1.gstatic.com/images?q=tbn:ANd9GcRI6C_T0Gc7gfNXs-sfvYYqxeoH2Mb-KnTPSAEZ3miDFYqjIkMZw68P",
 "welcomeMessage":"Hello that's me!"};
 var formattedName = HTMLheaderName.replace("%data%",bio.name);
 var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
@@ -13,4 +13,12 @@ $("#topContacts").append(formattedPhoneNumber);
 $("#topContacts").append(formattedEmail);
 var welcomeMessage =  HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage);
 var bioPic =  HTMLbioPic.replace("%data%",bio.pictureUrl);
-$("#header").append(bioPic);
+$("#header").append(bioPic); 
+$('#header').append(welcomeMessage);
+var skillsStart = HTMLskillsStart;
+$("#header").append(skillsStart);
+for (var i = 0; i<bio.skills.length; i++){
+	var skill = HTMLskills.replace("%data%",bio.skills[i]);
+	console.log(skill);
+	$("#skills").append(skill);
+}
